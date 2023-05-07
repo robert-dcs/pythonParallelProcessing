0ebfba0b5213ededd831a4eb2083012ee26321cc
Robertson Souza <robertson.dcs@gmail.com>
2023-05-07 16:33:31 -0300

    refactoring

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ simple_pool = psycopg2.pool.SimpleConnectionPool(
     database="postgres"
 )
 
-def create_staging_table():
+def drop_and_create_table():
     connection = simple_pool.getconn()
     connection.autocommit = True
     cursor = connection.cursor()
     cursor.execute("""
             DROP TABLE IF EXISTS persons;
-            CREATE UNLOGGED TABLE persons (
+            CREATE TABLE persons (
                 id SERIAL PRIMARY KEY,
                 Name varchar(255) NOT NULL
             );
@@ -29,48 +29,41 @@ def create_staging_table():
     simple_pool.putconn(connection)
     print("Table dropped and created")
 
-def convert_list(list):
+def convert_list_to_tuple(list):
     return tuple(list)
 
 def insert_person(person) -> None:
     connection = simple_pool.getconn()
     if connection:
-        sql = "INSERT INTO persons(Name) VALUES (%s)"
-
-        connection.autocommit = True
         try:
             cur = connection.cursor()
-            # execute the INSERT statement
             cur.execute('INSERT INTO persons (name) VALUES (%s)', person)
             connection.commit()
             simple_pool.putconn(connection)
         except (Exception, psycopg2.DatabaseError) as error:
-            print('Deu ruim na insercao', error)
+            print(error)
 
 def synchronous_processing(listOfPeople) -> None:
-    create_staging_table()
-    start = time.time()
+    drop_and_create_table()
+    start = (time.time() * 1000)
     for person in listOfPeople:
         insert_person(person)
-    end = time.time()
-    elapsedTime = end - start
-    print("Processed " + str(len(listOfPeople)) + " rows synchronous and took " + str(elapsedTime)  + " seconds")
+    end = (time.time() * 1000)
+    elapsed_time = end - start
+    print("Synchronous implementation took " + elapsed_time + " milliseconds and processed " + len(listOfPeople) + " records.\n")
 
 def parallel_processing(listOfPeople):
-    create_staging_table()
-    start = time.time()
-    print("Running threaded:")
+    drop_and_create_table()
+    start = (time.time() * 1000)
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
         for person in listOfPeople:
             futures.append(executor.submit(insert_person, [person]))
         for future in concurrent.futures.as_completed(futures):
             future.result()
-        end = time.time()
-        elapsedTime = end - start
-        print("Processed " + str(len(listOfPeople)) + " rows parallel and took " + str(elapsedTime) + " seconds")
-def format_person(person):
-    return str(person).replace("'", '"')
+        end = (time.time() * 1000)
+        elapsed_time = end - start
+        print("Synchronous implementation took " + elapsed_time + " milliseconds and processed " + len(listOfPeople) + " records.\n")
 
 if __name__ == '__main__':
     listOfPeople = []
@@ -78,16 +71,8 @@ if __name__ == '__main__':
     for person in wb['Sheet1'].iter_rows(values_only=True):
         listOfPeople.append(person)
 
-        # connection = psycopg2.connect(
-        #     host="localhost",
-        #     database="postgres",
-        #     user="postgres",
-        #     password=321,
-        # )
-     # connection.autocommit = True
-     # insert_execute_batch(connection, convert_list(listOfPeople))
-    synchronous_processing(convert_list(listOfPeople))
-    parallel_processing(convert_list(listOfPeople))
+    synchronous_processing(convert_list_to_tuple(listOfPeople))
+    parallel_processing(convert_list_to_tuple(listOfPeople))
     print("People saved.")
 
 
