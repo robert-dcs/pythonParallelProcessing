b1c10eaca9f670cb0f4a02da5d3e60526daec8c8
Robertson Souza <robertson.dcs@gmail.com>
2023-05-07 12:36:32 -0300

    fixing database pool connection and parallel execution

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ import openpyxl
 import psycopg2
 import psycopg2.extras
 import time
-from multiprocessing import cpu_count
-from multiprocessing import Pool
+import concurrent.futures
+from psycopg2 import pool
 
-def create_staging_table() -> None:
-    connection = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password=321,
-    )
+simple_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=20,
+    user="postgres",
+    password="321",
+    host="localhost",
+    port="5432",
+    database="postgres"
+)
+
+
+def create_staging_table():
+    connection = simple_pool.getconn()
     connection.autocommit = True
     cursor = connection.cursor()
     cursor.execute("""
-        DROP TABLE IF EXISTS persons;
-        CREATE UNLOGGED TABLE persons (
-            id SERIAL PRIMARY KEY,
-            Name varchar(255) NOT NULL
-        );
-    """)
+            DROP TABLE IF EXISTS persons;
+            CREATE UNLOGGED TABLE persons (
+                id SERIAL PRIMARY KEY,
+                Name varchar(255) NOT NULL
+            );
+        """)
+    simple_pool.putconn(connection)
     print("Table dropped and created")
 
 def convert_list(list):
@@ -28,40 +35,29 @@ def convert_list(list):
 
 def convert_string(string):
     return tuple(string)
-
 def insert_execute_batch(connection, listaDepessoas) -> None:
     with connection.cursor() as cursor:
         create_staging_table(cursor)
         psycopg2.extras.execute_batch(cursor, "INSERT INTO persons (Name) VALUES (%s)", listaDepessoas)
 
-def insert_person(person, conn=None) -> None:
+def insert_person(person) -> None:
 
-    sql = "INSERT INTO persons(Name) VALUES (%s)"
 
-    connection = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password=321,
-    )
+    connection = simple_pool.getconn()
+    if connection:
+        sql = "INSERT INTO persons(Name) VALUES (%s)"
 
-    connection.autocommit = True
-    name = person[0]
-    try:
-        cur = connection.cursor()
-        # execute the INSERT statement
-        cur.execute('INSERT INTO persons (name) VALUES (%s)', name)
-        print(person + "saved")
-        connection.commit()
-        connection.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(type(name))
-        print(name)
-        print('Eu ruim na insercao', error)
-    finally:
-        print('Connection closed')
-        if connection is not None:
-            connection.close()
+        connection.autocommit = True
+        try:
+            cur = connection.cursor()
+            # execute the INSERT statement
+            cur.execute('INSERT INTO persons (name) VALUES (%s)', person)
+            connection.commit()
+            simple_pool.putconn(connection)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print('Deu ruim na insercao', error)
+
+
 
 def synchronous_processing(listOfPeople) -> None:
     create_staging_table()
@@ -70,39 +66,40 @@ def synchronous_processing(listOfPeople) -> None:
         insert_person(person)
     end = time.time()
     elapsedTime = end - start
-    print("Processed " + len(listOfPeople) + " rows synchronous and took " + elapsedTime + " seconds")
+    print("Processed " + str(len(listOfPeople)) + " rows synchronous and took " + str(elapsedTime)  + " seconds")
 
 def parallel_processing(listOfPeople):
+    create_staging_table()
     start = time.time()
-    for person in listOfPeople:
-        pool = Pool(cpu_count())
-        pool.map(insert_person, person)
-        pool.close()
-        pool.join()
-
-    end = time.time()
-    elapsedTime = end - start
-    print("Processed " + len(listOfPeople) + " rows parallel and took " + elapsedTime + " seconds")
-
+    print("Running threaded:")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for person in listOfPeople:
+            futures.append(executor.submit(insert_person, [person]))
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+        end = time.time()
+        elapsedTime = end - start
+        print("Processed " + str(len(listOfPeople)) + " rows parallel and took " + str(elapsedTime) + " seconds")
 def format_person(person):
     return str(person).replace("'", '"')
 
 if __name__ == '__main__':
     listOfPeople = []
-    wb = openpyxl.load_workbook(filename="data.xlsx")
+    wb = openpyxl.load_workbook(filename="data1000000.xlsx")
     for person in wb['Sheet1'].iter_rows(values_only=True):
         listOfPeople.append(person)
 
-    connection = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password=321,
-    )
-    connection.autocommit = True
-    # insert_execute_batch(connection, convert_list(listOfPeople))
-    synchronous_processing(listOfPeople)
-    # parallel_processing(convert_list(listOfPeople))
+        # connection = psycopg2.connect(
+        #     host="localhost",
+        #     database="postgres",
+        #     user="postgres",
+        #     password=321,
+        # )
+     # connection.autocommit = True
+     # insert_execute_batch(connection, convert_list(listOfPeople))
+    synchronous_processing(convert_list(listOfPeople))
+    parallel_processing(convert_list(listOfPeople))
     print("People saved.")
 
 
